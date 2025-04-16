a236f4f0bb58ef949dd1ab0928534980d7249296
Zach Johnson <zachjohnson7887@gmail.com>
2025-04-15 20:39:59 -0400

    Removed deleted files

--- debug_funcs.cpp
+++
@@ -1,9 +0,0 @@
-#include"debug_funcs.h"
-
-void DBG_checkAllLayers(const std::vector<VkLayerProperties> &availableLayers) {
-	std::cout << "Available Layers : \n" << std::endl;
-
-	for (const auto& layer : availableLayers) {
-		std::cout << "\t" << layer.layerName << std::endl;
-	}
-}
