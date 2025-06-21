4ea54e612c0cf72eb895866471c035cd99305f7b
zachj78 <zachj78.codes@gmail.com>
2025-06-21 15:01:03 -0400

    Added basic Linux and MacOS build support- fixed macros

--- include/Utils/config.h
+++ include/Utils/config.h
@@ -1,16 +1,37 @@
 //GLOBAL CONFIG FILE
-//FOR GLOBAL VARIABLES AND LIBRARIES
+//FOR GLOBAL VARIABLES(try to minimize unless absolute necessary**) AND PREPROCESSOR MACROS
 
 #pragma once
 
-#define VK_USE_PLATFORM_WIN32_KHR
 #define GLFW_INCLUDE_VULKAN
-#define GLFW_EXPLORE_NATIVE_WIN32
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #define GLM_ENABLE_EXPERIMENTAL
 #define IMGUI_HAS_DOCK
 #define IMGUI_ENABLE_DOCKING
+
+//PLATFORM SPECIFIC MACROS -> VULKAN AND GLFW
+#if defined(_WIN32)
+	#pragma message("Detected Windows")
+	#define GLFW_EXPLORE_NATIVE_WIN32
+	#define VK_USE_PLATFORM_WIN32_KHR
+
+#elif defined(__APPLE__) && defined(__MACH__)
+	#pragma message("Detected MacOS")
+	#define VK_USE_PLATFORM_MACOS_MVK
+	#define GLFW_EXPOSE_NATIVE_COCOA
+
+//THIS NEEDS TO BE UPDATED TO CONDITIONALLY SUPPORT XCB AS WELL
+#elif defined(__linux__)
+	#pragma message("Detected Linux")
+	#define VK_USE_PLATFORM_WAYLAND_KHR
+	#define GLFW_EXPOSE_NATIVE_WAYLAND
+
+#else
+	#error "Unsupported Platform"
+
+#endif
+
 
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
@@ -37,7 +58,6 @@
 #include <set>
 #include <array>
 #include <string>
-#include <direct.h>
 #include <functional>
 #include <unordered_map>
 #include <utility>
@@ -45,13 +65,17 @@
 #include <fstream>
 #include <future>
 
+//Platform Specific Includes
+#if defined(_WIN32)
+	#include <direct.h>
+#else
+	#include <unistd.h>
+#endif
 
 #ifdef NDEBUG
 	constexpr bool enableValidationLayers = false;
 #else 
 	constexpr  bool enableValidationLayers = true;
+#endif
 
 constexpr int MAX_FRAMES_IN_FLIGHT = 2;
-
-
-#endif
