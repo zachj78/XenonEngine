3d129cb82c38537e9c30fa1e2a8b0a126890ed89
Zachary Johnson <zachj78.codes@gmail.com>
2025-07-08 17:16:49 -0400

    Shaders must be specified for each material, vertex attributes(meshes) will be constant

--- include/Managers/Material.h
+++ include/Managers/Material.h
@@ -5,14 +5,20 @@
 #include "Utils/config.h"
 #include "Builders/DescriptorBuilder.h"
 
+struct ShaderSet {
+    std::string vert; 
+    std::string frag; 
+};
+
 class Material {
 public:
     Material(std::string name,
         std::shared_ptr<Image> textureImage,
         VkDescriptorSetLayout descriptorSetLayout,
+        ShaderSet shaders, 
         VkDevice logicalDevice
     )
-        : name(name), textureImage(textureImage), logicalDevice(logicalDevice) {
+        : name(name), textureImage(textureImage), shaders(shaders), logicalDevice(logicalDevice) {
         std::cout << "Created material : " << name << std::endl;
     }
 
@@ -25,6 +31,7 @@ public:
     std::shared_ptr<Image> getTextureImage() const { return textureImage; };
     const std::string getName() const { return name; };
     std::vector<VkDescriptorSet> getDescriptorSets() { return descriptorSets; };
+    const ShaderSet getShaderSet() const { return shaders; };
 
 private:
 
@@ -33,6 +40,7 @@ private:
 
     std::string name;
     std::shared_ptr<Image> textureImage;
+    const ShaderSet shaders;
 
     //ONLY USED IF BINDLESS TEXTURES ARE NOT SUPPORTED
     std::vector<VkDescriptorSet> descriptorSets;
